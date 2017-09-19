bc50f1099d57dc8fea5d866c70798a7d3d7901d4
Martin Disch <martindisch@gmail.com>
2017-09-19 10:41:27 +0200

    Add saving and loading of file path

--- sshfiles/overview/views.py
+++ sshfiles/overview/views.py
@@ -1,3 +1,6 @@
+import os
+import json
+import fcntl
 from flask import Blueprint, render_template, request
 
 overview_blueprint = Blueprint('overview', __name__, static_folder='static',
@@ -6,4 +9,19 @@ overview_blueprint = Blueprint('overview', __name__, static_folder='static',
 
 @overview_blueprint.route('/', methods=['GET'])
 def overview_get():
+    """Show the overview page."""
+    if os.path.isfile('config.json'):
+        with open('config.json', 'r') as f:
+            fcntl.flock(f, fcntl.LOCK_EX)
+            kv = json.load(f)
+            return render_template('overview.html', path=kv['path'])
     return render_template('overview.html')
+
+@overview_blueprint.route('/', methods=['POST'])
+def overview_post():
+    """Update the file index."""
+    path = request.form['path']
+    with open('config.json', 'w') as f:
+        fcntl.flock(f, fcntl.LOCK_EX)
+        json.dump({'path': path}, f)
+    return render_template('overview.html', path=path)
